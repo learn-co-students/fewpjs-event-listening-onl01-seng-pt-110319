4635f8ba8dbbace72bf31b05911631d0a2d3bb19
Jordan <jordanleelong5@gmail.com>
2020-05-20 18:08:20 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+// addEventListener() takes two arguments:
+
+// the name of the event
+// a callback function to "handle" the event
+
+
 function addingEventListener() {
+    const input = document.getElementById('input');
+    input.addEventListener('click', function(event) {
+    alert('I was clicked yo');
+    })
+
+    
 }
+
+
